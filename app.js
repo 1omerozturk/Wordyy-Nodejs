75d1f6075297675518933f4fb4183cdd69027d1f
Ömer  ÖZTÜRK <omer.ozturk.or.ok@gmail.com>
2024-12-06 15:17:22 +0300

    update some packagse

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-require('dotenv').config()
 const connectDb = require('./config/dbConnection')
+require('dotenv').config()
 const express =require("express")
 const path = require('path')
 const cors = require('cors')
