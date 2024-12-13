75b100b47e15cd653240b2f932593322f6decd02
Ömer  ÖZTÜRK <omer.ozturk.or.ok@gmail.com>
2024-12-13 23:38:20 +0300

    Added admin panel

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,10 +2,14 @@ const express = require('express')
 const auth = require('../middleware/auth')
 const router = express.Router()
 const userController = require('../controllers/userController')
+const admin = require('../middleware/admin')
 
 router.post('/register', userController.registerUser)
+router.get('/users', admin, userController.getAllUsers)
+router.get('/otocreate', auth, userController.otoCreateWordy)
 router.get('/:id', auth, userController.getUser)
 router.post('/login', userController.loginUser)
 router.put('/update/:id', auth, userController.updateUser)
+router.delete('/:id', admin, userController.deleteUser)
 
 module.exports = router
